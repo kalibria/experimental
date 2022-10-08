06989fb961b20753e096d768a07e75d6d60ceae9
Maria Kazakova <mariakazakova92@gmail.com>
2022-10-08 13:40:51 +0300

    add json file withdata

--- rtk-query/src/App.js
+++ rtk-query/src/App.js
@@ -1,15 +1,29 @@
-import {useGetAllDataQuery} from './redux/dataApi';
+import {useGetAllDataQuery, useAddDataMutation} from './redux/dataApi';
 import {useState} from 'react';
 
 
 function App() {
 const [count, setCount] = useState('');
+const [newData, setNewData] = useState('')
   const {data=[], isLoading} = useGetAllDataQuery(count);
+  const [addData, {isError}] = useAddDataMutation();
+
+  const handleAddData = async () => {
+    if(newData){
+      await addData ({body: newData}).unwrap();
+      setNewData('')
+    }
+  }
 
   if(isLoading) return <h1>Loading...</h1>
 
   return (
     <div>
+      <div>
+        <input type="text" value={newData} onChange={(e)=>setNewData(e.target.value)}/>
+        <button onClick={handleAddData}>Add text</button>
+      </div>
+
       <div>
         <select value={count} onChange={(e) => setCount(e.target.value)}>
           <option value="">all</option>
@@ -18,6 +32,7 @@ const [count, setCount] = useState('');
           <option value="3">3</option>
         </select>
       </div>
+
       <ul>
         {data.map(item => (
           <li key={item.id}>
--- rtk-query/src/redux/dataApi.js
+++ rtk-query/src/redux/dataApi.js
@@ -3,14 +3,29 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const dataApi = createApi({
   reducerPath: 'dataApi',
+  tagTypes:['Text'],
   baseQuery: fetchBaseQuery({baseUrl: 'http://jsonplaceholder.typicode.com'}),
   endpoints: (builder) => ({
     getAllData: builder.query({
-      // query:(limit)=>'/posts'
-       query: (limit = '') => `/posts?${limit && `_limit=${limit}`}`
-    })
+       query: (limit = '') => `/posts?${limit && `_limit=${limit}`}`,
+      providesTags: (result) =>
+        result
+          ? [
+            ...result.map(({ id }) => ({ type: 'Text', id })),
+            { type: 'Text', id: 'LIST' },
+          ]
+          : [{ type: 'Text', id: 'LIST' }],
+    }),
+    addData: builder.mutation({
+      query:(body)=>( {
+        url: '/posts',
+        method: 'POST',
+        body,
+      }),
+      invalidatesTags: [{type: 'Text', id:'LIST'}]
+    }),
   })
 })
 
 
-export const { useGetAllDataQuery } = dataApi;
+export const { useGetAllDataQuery, useAddDataMutation } = dataApi;
