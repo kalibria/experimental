d92ca7bd3843eb6ab278830a652a03b594f213f5
Maria Kazakova <mariakazakova92@gmail.com>
2022-10-09 15:52:06 +0300

    add inputs from material-ui

--- formik/src/App.js
+++ formik/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () =>{
+  return  <Box
+    component="form"
+    sx={{
+      '& > :not(style)': { m: 1, width: '25ch' },
+    }}
+    noValidate
+    autoComplete="off"
+  >
+    <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+    <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+    <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+    <Button variant="contained">Contained</Button>
+
+  </Box>
 }
 
 export default App;
