9d7d1d59de4bd4d272d9a97e8e8ce59e52386ac8
Maria Kazakova <mariakazakova92@gmail.com>
2022-10-13 15:10:21 +0300

    use helper method called formik.getFieldProps()

--- formik/src/App.js
+++ formik/src/App.js
@@ -2,22 +2,47 @@ import React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import { useFormik } from 'formik';
 
 const App = () =>{
-  return  <Box
-    component="form"
-    sx={{
-      '& > :not(style)': { m: 1, width: '25ch' },
-    }}
-    noValidate
-    autoComplete="off"
-  >
-    <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-    <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-    <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-    <Button variant="contained">Contained</Button>
+  const formik = useFormik({
+    initialValues:{
+      login:'',
+      password:'',
+      email:''
+    },
+    onSubmit: (values) =>{
+      alert(JSON.stringify(values))
+    }
+  })
 
-  </Box>
+
+
+  return (
+    <React.Fragment >
+      <h1>React Hook Form</h1>
+
+      <Box
+        component="form"
+        sx={{
+          '& > :not(style)': { m: 1, width: '25ch' },
+        }}
+        noValidate
+        autoComplete="off"
+        onSubmit={formik.handleSubmit}
+      >
+        <TextField id="outlined-basic" label="Login" variant="outlined" onChange={formik.handleChange} {...formik.getFieldProps('login')}/>
+        <TextField id="outlined-basic" label="Password" variant="outlined" onChange={formik.handleChange} {...formik.getFieldProps('password')}/>
+        <TextField id="outlined-basic" label="Email" variant="outlined" onChange={formik.handleChange} {...formik.getFieldProps('email')}/>
+        <Button variant="contained">Submit</Button>
+
+
+
+      </Box>
+    </React.Fragment>
+    )
+
+
 }
 
 export default App;
