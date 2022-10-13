45975ac736753dd8b9a15cb24738cbe6bf9053c7
Maria Kazakova <mariakazakova92@gmail.com>
2022-10-13 16:05:36 +0300

    use component <Formik> instead of hook useFormik

--- formik/src/App.js
+++ formik/src/App.js
@@ -2,26 +2,27 @@ import React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
-import { useFormik } from 'formik';
+import {Formik, useField} from 'formik';
+
+
+
+const MyTextInput = ({label, id, variant, ...props}) =>{
+  const [field,meta] = useField(props)
+  return(
+    <TextField label={label} id={id} variant={variant} {...field} {...props}/>
+  )
+}
+
+
 
 const App = () =>{
-  const formik = useFormik({
-    initialValues:{
-      login:'',
-      password:'',
-      email:''
-    },
-    onSubmit: (values) =>{
-      alert(JSON.stringify(values))
-    }
-  })
-
-
 
   return (
-    <React.Fragment >
+    <React.Fragment>
       <h1>React Hook Form</h1>
-
+      <Formik initialValues={{login:'', password:'',email:''}} onSubmit={(values)=>{
+  alert(JSON.stringify(values))
+}}>
       <Box
         component="form"
         sx={{
@@ -29,16 +30,18 @@ const App = () =>{
         }}
         noValidate
         autoComplete="off"
-        onSubmit={formik.handleSubmit}
       >
-        <TextField id="outlined-basic" label="Login" variant="outlined" onChange={formik.handleChange} {...formik.getFieldProps('login')}/>
-        <TextField id="outlined-basic" label="Password" variant="outlined" onChange={formik.handleChange} {...formik.getFieldProps('password')}/>
-        <TextField id="outlined-basic" label="Email" variant="outlined" onChange={formik.handleChange} {...formik.getFieldProps('email')}/>
+        <MyTextInput id="outlined-basic" label="Login" variant="outlined" name="login"/>
+        <MyTextInput id="outlined-basic" label="Password" variant="outlined" name='password'/>
+        <MyTextInput id="outlined-basic" label="Email" variant="outlined" name='email'/>
+
+
         <Button variant="contained">Submit</Button>
 
 
 
       </Box>
+    </Formik>
     </React.Fragment>
     )
 
