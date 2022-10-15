0d9697a84ae1ea01b44112b804e5e3cd40413304
Maria Kazakova <mariakazakova92@gmail.com>
2022-10-15 13:43:59 +0300

    use Jason Quense’s library Yup for object schema validation

--- formik/src/App.js
+++ formik/src/App.js
@@ -2,50 +2,84 @@ import React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
-import {Formik, useField} from 'formik';
+import { Formik, useField } from 'formik';
+import * as Yup from 'yup';
 
+const MyTextInput = ({ label, id, variant, ...props }) => {
+  const [field, meta] = useField(props);
+  return (
+    <>
+      <TextField
+        label={label}
+        id={id}
+        variant={variant}
+        {...field}
+        {...props}
+      />
+      {meta.touched && meta.error ? (
+        <div className="error">{meta.error}</div>
+      ) : null}
+    </>
+  );
+};
 
-
-const MyTextInput = ({label, id, variant, ...props}) =>{
-  const [field,meta] = useField(props)
-  return(
-    <TextField label={label} id={id} variant={variant} {...field} {...props}/>
-  )
-}
-
-
-
-const App = () =>{
-
+const App = () => {
   return (
     <React.Fragment>
       <h1>React Hook Form</h1>
-      <Formik initialValues={{login:'', password:'',email:''}} onSubmit={(values)=>{
-  alert(JSON.stringify(values))
-}}>
-      <Box
-        component="form"
-        sx={{
-          '& > :not(style)': { m: 1, width: '25ch' },
+      <Formik
+        initialValues={{ login: '', password: '', email: '' }}
+        validationSchema={Yup.object({
+          login: Yup.string()
+            .max(10, 'Must be 10 characters or less')
+            .required('Required'),
+          password: Yup.string()
+            // .min(3, 'Must be at least 3 characters')
+            .max(10, 'Must be 10 characters or less ')
+            .required('Required'),
+          email: Yup.string()
+            .email('Invalid email address')
+            .required('Required'),
+        })}
+        onSubmit={(values, { setSubmitting }) => {
+          alert(JSON.stringify(values));
+          setSubmitting(false);
         }}
-        noValidate
-        autoComplete="off"
       >
-        <MyTextInput id="outlined-basic" label="Login" variant="outlined" name="login"/>
-        <MyTextInput id="outlined-basic" label="Password" variant="outlined" name='password'/>
-        <MyTextInput id="outlined-basic" label="Email" variant="outlined" name='email'/>
+        <Box
+          component="form"
+          sx={{
+            '& > :not(style)': { m: 1, width: '25ch' },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <MyTextInput
+            id="outlined-basic"
+            label="Login"
+            variant="outlined"
+            name="login"
+          />
+          <MyTextInput
+            id="outlined-basic"
+            label="Password"
+            variant="outlined"
+            name="password"
+          />
+          <MyTextInput
+            id="outlined-basic"
+            label="Email"
+            variant="outlined"
+            name="email"
+          />
 
-
-        <Button variant="contained">Submit</Button>
-
-
-
-      </Box>
-    </Formik>
+          <Button variant="contained" type="submit">
+            Submit
+          </Button>
+        </Box>
+      </Formik>
     </React.Fragment>
-    )
-
-
-}
+  );
+};
 
 export default App;
